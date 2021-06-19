4b79d0a12f0b97e7ddb54afb18388cab85efbdb7
Yevhenii Herasymenko <74263798+Zhekager@users.noreply.github.com>
2021-06-19 16:10:23 +0300

    дприбирає коміти index.js

--- src/index.js
+++ src/index.js
@@ -1,58 +1,6 @@
 import './sass/main.scss';
-
 import './js/showTrendingMovies';
-
 import './js/upScrol';
-import getRefs from './js/refs';
-import FilmApiService from './js/apiService.js'
-import markUpFilmCardTpl from './templates/film.hbs'
-
-const refs = getRefs();
-
-const filmsApiService = new FilmApiService();
-
-refs.searchInput.addEventListener('input', onSearch);
-
-let galleryRef = document.querySelector('#gallery');
-    filmsApiService.fetchTrendingMovies()
-    .then(filmData => {
-    let result = filmData.results
-    createFilmCardsMarkUp(result);
-    })
-    .catch(error => console.log('error', error));
-            
-function onSearch(e) {
-    e.preventDefault();
-    filmsApiService.searchQuery = e.target.value;
-
-    if (filmsApiService.searchQuery === '') {
-              filmsApiService.fetchTrendingMovies()
-            .then(filmData => {
-                let result = filmData.results
-        createFilmCardsMarkUp(result);
-    })
-            .catch(error => console.log('error', error));
-    };
-
-    filmsApiService.resetPage()
-
-    clearImagesContainer();
-
-    filmsApiService.fetchFilms()
-            .then(filmData => {
-                let result = filmData.results
-        createFilmCardsMarkUp(result);
-    })
-    .catch(error => console.log('error', error));
-};
-
-function createFilmCardsMarkUp(films) {
-    galleryRef.insertAdjacentHTML('beforeend', markUpFilmCardTpl(films));
-}
-
-function clearImagesContainer() {
-  galleryRef.innerHTML = '';
-}
 
 
 
