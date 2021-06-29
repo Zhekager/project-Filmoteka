2e35a240cbd47d318b4378c1098253718f23c1ef
Nata Pryhunova <prygunova@gmail.com>
2021-06-30 01:52:22 +0300

    Добавляет аватар

--- src/js/authFireBaseConfig.js
+++ src/js/authFireBaseConfig.js
@@ -8,46 +8,55 @@ const firebaseConfig = {
 };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-  
-let provider = new firebase.auth.GoogleAuthProvider();
 
-document.getElementById('dashboard').style.display="none"
+// document.getElementById('dashboard').style.display="none"
 document.getElementById('login').addEventListener('click', GoogleLogin)
 document.getElementById('logout').addEventListener('click', LogoutUser)
 
+const provider = new firebase.auth.GoogleAuthProvider()
+
       function GoogleLogin(){
-        
+        console.log('Login Btn Call')
         firebase.auth().signInWithPopup(provider).then(res=>{
           console.log(res.user)
-          document.getElementById('LoginScreen').style.display="none"
-          document.getElementById('dashboard').style.display="block"
+          document.getElementById('login').style.display="none"
+          document.getElementById('userDetails').style.display = "block"
+            document.getElementById('logout').style.display="none"
           showUserDetails(res.user)
         }).catch(e=>{
           console.log(e)
         })
       }
 
-      // function showUserDetails(user){
-      //   document.getElementById('userDetails').innerHTML = `
-      //     <img src="${user.photoURL}" style="width:55px; border-radius: 50%; margin-bottom: 3px;">`
-      // }
+      function showUserDetails(user){
+        document.getElementById('userDetails').innerHTML = `
+          <img class="user-img" src="${user.photoURL}"> 
+            <div class="user-block">
+              <p class="user-name">${user.displayName}</p>
+              <p class="user-email">${user.email}</p>
+            </div>
+        `
+      }
 
       function checkAuthState(){
         firebase.auth().onAuthStateChanged(user=>{
           if(user){
-            document.getElementById('LoginScreen').style.display = "none"
-            document.getElementById('dashboard').style.display="block"
-            // showUserDetails(user)
-          } else {           
+            document.getElementById('login').style.display = "none"
+            document.getElementById('userDetails').style.display = "block"
+            document.getElementById('logout').style.display="block"
+            // document.getElementById('dashboard').style.display="block"
+            showUserDetails(user)
+          }else{            
           }
         })
       }
 
       function LogoutUser(){
-      
+        console.log('Logout Btn Call')
         firebase.auth().signOut().then(()=>{
-          document.getElementById('LoginScreen').style.display="block"
-          document.getElementById('dashboard').style.display="none"
+          document.getElementById('login').style.display="block"
+          document.getElementById('userDetails').style.display = "none"
+          document.getElementById('logout').style.display="none"
         }).catch(e=>{
           console.log(e)
         })
